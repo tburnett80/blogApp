121cb5aad9566fafe152f1745bc2b8e5b8ac0d7e
Tim <tburnett80@gmail.com>
2018-12-27 18:46:36 -0600

    Start wiring up basic API to add rough UI

---
+++ BlogApp/BlogApp.Server/Controllers/BlogPostController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using BlogApp.Common.Contracts.Managers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.Server.Controllers
+{
+    [ApiController]
+    public class BlogPostController : ControllerBase
+    {
+        #region constructor and private members
+        private readonly IBlogPostManager _manager;
+
+        public BlogPostController(IBlogPostManager manager)
+        {
+            _manager = manager
+                ?? throw new ArgumentNullException(nameof(manager));
+        }
+        #endregion
+
+        [Route("blog/tags/v1")]
+        [HttpGet]
+        public async Task<IActionResult> GetTags()
+        {
+            return Ok(await _manager.GetTagList());
+        }
+    }
+}
--- BlogApp/BlogApp.Server/Startup.cs
+++ BlogApp/BlogApp.Server/Startup.cs
@@ -1,16 +1,25 @@
-﻿using Microsoft.AspNetCore.Blazor.Server;
+﻿using System;
+using Microsoft.AspNetCore.Blazor.Server;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.DependencyInjection;
-using Newtonsoft.Json.Serialization;
 using System.Linq;
 using System.Net.Mime;
+using Microsoft.Extensions.Configuration;
 
 namespace BlogApp.Server
 {
     public class Startup
     {
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config
+                ?? throw new ArgumentNullException(nameof(config));
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -25,6 +34,8 @@ namespace BlogApp.Server
                     WasmMediaTypeNames.Application.Wasm,
                 });
             });
+            
+            IoC.IoCBootstrap.AddServices(services, _config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
